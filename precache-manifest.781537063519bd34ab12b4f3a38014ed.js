eb9a31931c5a6f8ed2db8fb59283d776821b142d
activecodar <activecodar@gmail.com>
2023-06-09 22:49:38 +0300

    Updates

---
+++ precache-manifest.781537063519bd34ab12b4f3a38014ed.js
@@ -0,0 +1,346 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7254298fc08baa6d1cf43857454a9312",
+    "url": "/propertyapp/index.html"
+  },
+  {
+    "revision": "c37ab9d748a799778ece",
+    "url": "/propertyapp/static/css/10.99ccfae6.chunk.css"
+  },
+  {
+    "revision": "294165903d4aec6630b5",
+    "url": "/propertyapp/static/css/4.c61ffb77.chunk.css"
+  },
+  {
+    "revision": "4e0d14d1c71c00f1a208",
+    "url": "/propertyapp/static/css/5.ad76a385.chunk.css"
+  },
+  {
+    "revision": "8e727fadb140d926992b",
+    "url": "/propertyapp/static/css/8.ac98d7c3.chunk.css"
+  },
+  {
+    "revision": "b4c397a6ffcea371fa02",
+    "url": "/propertyapp/static/css/main.567fd414.chunk.css"
+  },
+  {
+    "revision": "9884aa3c4cb570061999",
+    "url": "/propertyapp/static/js/0.11686cf1.chunk.js"
+  },
+  {
+    "revision": "fd7525d544dd9c67d07855cb8778e590",
+    "url": "/propertyapp/static/js/0.11686cf1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de0464afa87cf6e0b8c3",
+    "url": "/propertyapp/static/js/1.ceff2be8.chunk.js"
+  },
+  {
+    "revision": "c37ab9d748a799778ece",
+    "url": "/propertyapp/static/js/10.a73bd08a.chunk.js"
+  },
+  {
+    "revision": "ad2bf1ed9124616e1eef4549d94ac47e",
+    "url": "/propertyapp/static/js/10.a73bd08a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "17c436e7493c825a44a7",
+    "url": "/propertyapp/static/js/11.654888b5.chunk.js"
+  },
+  {
+    "revision": "abb2dfb31b708947a791",
+    "url": "/propertyapp/static/js/12.e696f257.chunk.js"
+  },
+  {
+    "revision": "190ad7e92f7e62a0fc9f",
+    "url": "/propertyapp/static/js/13.a33e8817.chunk.js"
+  },
+  {
+    "revision": "b7d7e662c6b5be7a1d89",
+    "url": "/propertyapp/static/js/14.6f9625a1.chunk.js"
+  },
+  {
+    "revision": "083077ca56ad5b4f777c",
+    "url": "/propertyapp/static/js/15.2303740b.chunk.js"
+  },
+  {
+    "revision": "d2145d117a9db4a9d7c6",
+    "url": "/propertyapp/static/js/16.b629ed6a.chunk.js"
+  },
+  {
+    "revision": "3dfa94418b41435c0b2f",
+    "url": "/propertyapp/static/js/17.ff6e13ef.chunk.js"
+  },
+  {
+    "revision": "38b373e5db29d22a06ec",
+    "url": "/propertyapp/static/js/18.29208af3.chunk.js"
+  },
+  {
+    "revision": "0fbad6ca8663baa0c77d",
+    "url": "/propertyapp/static/js/19.584089a7.chunk.js"
+  },
+  {
+    "revision": "8aa22d0c9d4f59029965",
+    "url": "/propertyapp/static/js/2.80c27da9.chunk.js"
+  },
+  {
+    "revision": "c9b6430f6bb3d5c3c12c",
+    "url": "/propertyapp/static/js/20.23e27036.chunk.js"
+  },
+  {
+    "revision": "594fa061b6eebe07bb36",
+    "url": "/propertyapp/static/js/21.4a5344c5.chunk.js"
+  },
+  {
+    "revision": "84853479c560bc09f91e",
+    "url": "/propertyapp/static/js/22.0ea84e38.chunk.js"
+  },
+  {
+    "revision": "1690e476ea1da66c6958",
+    "url": "/propertyapp/static/js/23.5d77c17b.chunk.js"
+  },
+  {
+    "revision": "16129a4ff5a5360aa199",
+    "url": "/propertyapp/static/js/24.e9e7f1da.chunk.js"
+  },
+  {
+    "revision": "d9c93f2c4e12c47e176d",
+    "url": "/propertyapp/static/js/25.f6ad36d4.chunk.js"
+  },
+  {
+    "revision": "64b9e782df2466fb9fcd",
+    "url": "/propertyapp/static/js/26.067f114e.chunk.js"
+  },
+  {
+    "revision": "fe19fbfcd29ed2b6871e",
+    "url": "/propertyapp/static/js/27.a9f24182.chunk.js"
+  },
+  {
+    "revision": "8b1bf61c62fad0c13c38",
+    "url": "/propertyapp/static/js/28.e47d7dc0.chunk.js"
+  },
+  {
+    "revision": "72cfe28b8e5ff2ec1833",
+    "url": "/propertyapp/static/js/29.b8573229.chunk.js"
+  },
+  {
+    "revision": "5ecb1479c1612322482f",
+    "url": "/propertyapp/static/js/3.1528240d.chunk.js"
+  },
+  {
+    "revision": "58d7250b12c6a70f069c",
+    "url": "/propertyapp/static/js/30.6f1d53e0.chunk.js"
+  },
+  {
+    "revision": "d34fb272cd9467d6564c",
+    "url": "/propertyapp/static/js/31.06f673bf.chunk.js"
+  },
+  {
+    "revision": "1493cffb2ef21b3b4a0f",
+    "url": "/propertyapp/static/js/32.49054407.chunk.js"
+  },
+  {
+    "revision": "cd05f66649abfa8d8050",
+    "url": "/propertyapp/static/js/33.373891f5.chunk.js"
+  },
+  {
+    "revision": "fcb7502da70bdd3eaf91",
+    "url": "/propertyapp/static/js/34.b12d6056.chunk.js"
+  },
+  {
+    "revision": "2aac3843a01f3a469a4c",
+    "url": "/propertyapp/static/js/35.c9e2d55f.chunk.js"
+  },
+  {
+    "revision": "fe867cf51fa3ac23a1c5",
+    "url": "/propertyapp/static/js/36.c5075f63.chunk.js"
+  },
+  {
+    "revision": "083a7484456855696872",
+    "url": "/propertyapp/static/js/37.f0f6ab0c.chunk.js"
+  },
+  {
+    "revision": "d51bbf6178bdbc6c23b9",
+    "url": "/propertyapp/static/js/38.6c3f6f3d.chunk.js"
+  },
+  {
+    "revision": "14fcd248112a123bd2c6",
+    "url": "/propertyapp/static/js/39.5608b929.chunk.js"
+  },
+  {
+    "revision": "294165903d4aec6630b5",
+    "url": "/propertyapp/static/js/4.9f2085d1.chunk.js"
+  },
+  {
+    "revision": "94d345d07f28e397ce0d275d9e7db62d",
+    "url": "/propertyapp/static/js/4.9f2085d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f35da32aad331069edd8",
+    "url": "/propertyapp/static/js/40.5c5e9675.chunk.js"
+  },
+  {
+    "revision": "a6456c7d601aa1af68f9",
+    "url": "/propertyapp/static/js/41.63118597.chunk.js"
+  },
+  {
+    "revision": "18021fa32ae6b4fd85f0",
+    "url": "/propertyapp/static/js/42.cd406b80.chunk.js"
+  },
+  {
+    "revision": "084f867d16f0a6b93247",
+    "url": "/propertyapp/static/js/43.65666ca2.chunk.js"
+  },
+  {
+    "revision": "d2e13de31216f76fbbb9",
+    "url": "/propertyapp/static/js/44.0da1d22d.chunk.js"
+  },
+  {
+    "revision": "fa0467ed20caf75765de",
+    "url": "/propertyapp/static/js/45.51c4fbef.chunk.js"
+  },
+  {
+    "revision": "e34919497dc4e2dc8434",
+    "url": "/propertyapp/static/js/46.a7f79299.chunk.js"
+  },
+  {
+    "revision": "d90aaf64d51e21073a22",
+    "url": "/propertyapp/static/js/47.fe403764.chunk.js"
+  },
+  {
+    "revision": "32bc8560f2c05efb1989",
+    "url": "/propertyapp/static/js/48.87d0f969.chunk.js"
+  },
+  {
+    "revision": "b297c8ff8c1ef9393ee8",
+    "url": "/propertyapp/static/js/49.f34c2ac8.chunk.js"
+  },
+  {
+    "revision": "4e0d14d1c71c00f1a208",
+    "url": "/propertyapp/static/js/5.299d4580.chunk.js"
+  },
+  {
+    "revision": "44a044edc07ffb95a82853e75b87eb2b",
+    "url": "/propertyapp/static/js/5.299d4580.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b96d5f3048aad0a47166",
+    "url": "/propertyapp/static/js/50.ffd7b585.chunk.js"
+  },
+  {
+    "revision": "d0404e499733c0efe7d5",
+    "url": "/propertyapp/static/js/51.6b5944bb.chunk.js"
+  },
+  {
+    "revision": "1ddb79c5e82112a58898",
+    "url": "/propertyapp/static/js/52.1dfd1bc2.chunk.js"
+  },
+  {
+    "revision": "ea42eb6e7aba16f8b792",
+    "url": "/propertyapp/static/js/53.f1e9172b.chunk.js"
+  },
+  {
+    "revision": "543de9823ff8e29eb6b9",
+    "url": "/propertyapp/static/js/54.0393c45f.chunk.js"
+  },
+  {
+    "revision": "8e727fadb140d926992b",
+    "url": "/propertyapp/static/js/8.70ca4f79.chunk.js"
+  },
+  {
+    "revision": "beb0d9d10d332f2eab65d5b49e069e1f",
+    "url": "/propertyapp/static/js/8.70ca4f79.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f8cd08e4370acadd5fb2",
+    "url": "/propertyapp/static/js/9.46c7f1d1.chunk.js"
+  },
+  {
+    "revision": "b4c397a6ffcea371fa02",
+    "url": "/propertyapp/static/js/main.d4483ebb.chunk.js"
+  },
+  {
+    "revision": "7460eab7babfd61117f0",
+    "url": "/propertyapp/static/js/runtime-main.c24ef71d.js"
+  },
+  {
+    "revision": "4dfdd1c035c641accc4f3fedb7b73055",
+    "url": "/propertyapp/static/media/lato-latin-100.4dfdd1c0.woff"
+  },
+  {
+    "revision": "9ca14b8703044ec44627982d6d0e1e44",
+    "url": "/propertyapp/static/media/lato-latin-100.9ca14b87.woff2"
+  },
+  {
+    "revision": "83a45512c894889710e9966b9edddc6d",
+    "url": "/propertyapp/static/media/lato-latin-100italic.83a45512.woff"
+  },
+  {
+    "revision": "de3428c265cecf1856805f21f274bb82",
+    "url": "/propertyapp/static/media/lato-latin-100italic.de3428c2.woff2"
+  },
+  {
+    "revision": "656963c4beb814e754e38478fcaa0439",
+    "url": "/propertyapp/static/media/lato-latin-300.656963c4.woff"
+  },
+  {
+    "revision": "98d8cf792834c0bef59c2be99dc3533d",
+    "url": "/propertyapp/static/media/lato-latin-300.98d8cf79.woff2"
+  },
+  {
+    "revision": "8e90b967ea69fc68b130d36cc34d41c0",
+    "url": "/propertyapp/static/media/lato-latin-300italic.8e90b967.woff"
+  },
+  {
+    "revision": "a21767e20d27a9c06007c981a8e5f827",
+    "url": "/propertyapp/static/media/lato-latin-300italic.a21767e2.woff2"
+  },
+  {
+    "revision": "b4d2c4c39853ee244272c04999b230ba",
+    "url": "/propertyapp/static/media/lato-latin-400.b4d2c4c3.woff2"
+  },
+  {
+    "revision": "b8ee546acd6cc0c49f42ad3d48ef244f",
+    "url": "/propertyapp/static/media/lato-latin-400.b8ee546a.woff"
+  },
+  {
+    "revision": "117e995c97eab30fb92843616018d1f1",
+    "url": "/propertyapp/static/media/lato-latin-400italic.117e995c.woff2"
+  },
+  {
+    "revision": "e652470b9572b86907b77a97f40dc915",
+    "url": "/propertyapp/static/media/lato-latin-400italic.e652470b.woff"
+  },
+  {
+    "revision": "1efbd38aa76ddae2580fedf378276333",
+    "url": "/propertyapp/static/media/lato-latin-700.1efbd38a.woff2"
+  },
+  {
+    "revision": "874b8e7bc7e8d1507b50f56bc6c9b536",
+    "url": "/propertyapp/static/media/lato-latin-700.874b8e7b.woff"
+  },
+  {
+    "revision": "5ce0990b59e1ed40e05327f625a24c4a",
+    "url": "/propertyapp/static/media/lato-latin-700italic.5ce0990b.woff2"
+  },
+  {
+    "revision": "9e9a574493d4cd2fa62380f376461941",
+    "url": "/propertyapp/static/media/lato-latin-700italic.9e9a5744.woff"
+  },
+  {
+    "revision": "947e87c53b5765bfc8982613ccd789e9",
+    "url": "/propertyapp/static/media/lato-latin-900.947e87c5.woff2"
+  },
+  {
+    "revision": "a82ff6ac9208656f9a21a65dfacbcae3",
+    "url": "/propertyapp/static/media/lato-latin-900.a82ff6ac.woff"
+  },
+  {
+    "revision": "73d7130e1dc196d25bb35faf2afbbb43",
+    "url": "/propertyapp/static/media/lato-latin-900italic.73d7130e.woff"
+  },
+  {
+    "revision": "bd86aaa72d23c91de85390a93aa6bef2",
+    "url": "/propertyapp/static/media/lato-latin-900italic.bd86aaa7.woff2"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ece9c6ec10691394a6a488ed6148161e.js"
+  "/propertyapp/precache-manifest.781537063519bd34ab12b4f3a38014ed.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/propertyapp/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
